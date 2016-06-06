1566f383dd55e81e9a8de8d0c8b4800c4476508a
tankorsmash <tankorsmash@gmail.com>
2016-06-05 22:05:46 -0700

    temp

--- hbd/views.py
+++ hbd/views.py
@@ -69,3 +69,11 @@ class TodayView(TemplateView):
 		cheers.save()
 
 		return self.get(request, *args, **kwargs)
+
+class ProfileView(TemplateView):
+	template_name = "profile.html"
+
+	def get_context_data(self, **kwargs):
+		context = super(ProfileView, self).get_context_data(**kwargs)
+		context
+		return context
